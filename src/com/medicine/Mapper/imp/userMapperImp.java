6d5390923ed8406f2ba2082395b06722a1a1da6c
lingdianshiren <2077358365@qq.com>
2024-01-17 10:13:34 +0800

    fix UserFrame encode

--- src/com/medicine/Mapper/imp/userMapperImp.java
+++ src/com/medicine/Mapper/imp/userMapperImp.java
@@ -1,4 +1,25 @@
 package com.medicine.Mapper.imp;
+import com.medicine.Entity.User;
+import com.medicine.Mapper.userMapper;
+public class userMapperImp implements userMapper{
+    @Override
+    public boolean insert(User u) {
+//        String sql ="INSERT INTO user(username,password)"
+        return false;
+    }
 
-public class userMapperImp {
+    @Override
+    public boolean delete(User u) {
+        return false;
+    }
+
+    @Override
+    public boolean update(User u) {
+        return false;
+    }
+
+    @Override
+    public User[] select(String key) {
+        return new User[0];
+    }
 }
