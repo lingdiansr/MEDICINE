3e17cbd212c034de203c18a54fd69d198bfa071b
lingdianshiren <2077358365@qq.com>
2024-01-17 09:46:10 +0800

    test

--- src/com/medicine/Mapper/imp/userImp.java
+++ src/com/medicine/Mapper/imp/userImp.java
@@ -1,10 +1,10 @@
 package com.medicine.Mapper.imp;
-import com.medicine.Entity.user;
+import com.medicine.Entity.User;
 
 public interface userImp {
-    boolean insert(user u);
-    boolean delete(user u);
-    boolean update(user u);
-    user[] select(String key);
+    boolean insert(User u);
+    boolean delete(User u);
+    boolean update(User u);
+    User[] select(String key);
 
 }
