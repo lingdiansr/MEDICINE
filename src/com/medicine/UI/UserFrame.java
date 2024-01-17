29367120f8b7d1cf70a210ca62d7cd6c550873db
HRruiH <2311465445@qq.com>
2024-01-17 10:06:46 +0800

    修改了登录界面

--- src/com/medicine/UI/UserFrame.java
+++ src/com/medicine/UI/UserFrame.java
@@ -5,82 +5,101 @@ import java.awt.*;
 
 public class UserFrame extends JFrame {
     JPanel panel;
+
     private JLabel titleLabel;
+
     private JLabel usernameLabel;
+
     private JLabel passwordLabel;
-    private  JTextField usernameField;
-    private JPasswordField passwordField;
+
+    private JTextField username;
+
+    private JPasswordField password;
+
     private JButton loginButton;
-    private  JButton registerButton;
-    public UserFrame(){
-        initFrame();
+    private JButton registerButton;
+
+    public UserFrame() {
         initPanel();
+        initFrame();
         loginButtonListener();
         registerButtonListener();
     }
-    private  void registerButtonListener(){
+
+    private void registerButtonListener() {
 
     }
-    private  void loginButtonListener(){
+
+    private void loginButtonListener() {
 
     }
-    private void initFrame(){
-        this.setTitle("登录");
-        this.setSize(800,500);
-        this.setLocationRelativeTo(null);
-        this.setVisible(true);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    }
-    private void initPanel(){
-        panel=new JPanel();
+
+    private void initPanel() {
+        panel = new JPanel();
         this.setContentPane(panel);
         this.setLayout(null);
-
-        titleLabel=new JLabel("医药管理系统",SwingConstants.CENTER);
+        // 1. 标题标签
+        titleLabel = new JLabel("医药管理系统",SwingConstants.CENTER);
         titleLabel.setForeground(Color.BLUE);
-        titleLabel.setFont(new Font("宋体",Font.PLAIN,30));
+        titleLabel.setFont(new Font("宋体",Font.PLAIN,50));
         titleLabel.setSize(800,100);
 
-        usernameLabel=new JLabel("用户名:");
+        // 2.用户名标签
+        usernameLabel = new JLabel("用户名:");
         usernameLabel.setForeground(new Color(0xFF0000));
-        usernameLabel.setFont(new Font("宋体",Font.PLAIN,30));
-        usernameLabel.setBounds(200,160,200,100);
+        usernameLabel.setFont(new Font("宋体", Font.PLAIN, 30));
+        usernameLabel.setBounds(200, 100, 200, 100);
 
-        passwordLabel=new JLabel("密码:");
+        // 3.登录标签
+        passwordLabel = new JLabel("密码  :");
         passwordLabel.setForeground(new Color(0xFF0000));
-        passwordLabel.setFont(new Font("宋体 ",Font.PLAIN,30));
-        passwordLabel.setBounds(200,160,200,100);
+        passwordLabel.setFont(new Font("宋体", Font.PLAIN, 30));
+        passwordLabel.setBounds(200, 160, 200, 100);
 
         panel.add(titleLabel);
         panel.add(usernameLabel);
         panel.add(passwordLabel);
 
-        usernameField =new JTextField(20);
-        usernameField.setFont(new Font("宋体",Font.PLAIN,18));
-        usernameField.setSelectedTextColor(new Color(0xFF0000));
-        usernameField.setBounds(330,132,280,40);
+        // 字段值样式处理
+        username = new JTextField(20);
+        username.setFont(new Font("宋体",Font.PLAIN,18));
+        username.setSelectedTextColor(new Color(0xFF0000));
+        username.setBounds(330,132,280,40);
 
-        passwordField =new JPasswordField(20);
-        passwordField.setFont(new Font("宋体",Font.PLAIN,18));
-        passwordField.setSelectedTextColor(new Color(0xFF0000));
-        passwordField.setBounds(330,192,200,40);
+        password = new JPasswordField(20);
+        password.setBounds(330, 192, 280, 40);
+        panel.add(username);
+        panel.add(password);
 
-        panel.add(usernameField);
-        panel.add(passwordField);
-
-        loginButton =new JButton("登录");
+        // 处理按钮
+        // 登录
+        loginButton = new JButton("登录");
         loginButton.setForeground(Color.BLUE);
-        loginButton.setFont(new Font("宋体",Font.PLAIN,20));
+        loginButton.setBackground(Color.GREEN);
+        loginButton.setFont(new Font("宋体", Font.PLAIN, 20));
         loginButton.setBorderPainted(false);
-        loginButton.setBounds(270,300,100,50);
-
-        registerButton =new JButton("注册");
+        loginButton.setBounds(270, 300, 100, 50);
+        //注册
+        registerButton = new JButton("注册");
         registerButton.setForeground(Color.BLUE);
-        registerButton.setFont(new Font("宋体",Font.PLAIN,20));
+        registerButton.setBackground(Color.PINK);
+        registerButton.setFont(new Font("宋体", Font.PLAIN, 20));
         registerButton.setBorderPainted(false);
-        registerButton.setBounds(270,300,100,50);
-
+        registerButton.setBounds(450, 300, 100, 50);
         panel.add(loginButton);
         panel.add(registerButton);
+    }
+
+    private void initFrame() {
+        this.setTitle("登录");
+        this.setSize(800, 500);
+        this.setLocationRelativeTo(null);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.setVisible(true);
+    }
+
+    public static void  main(String[] args){
+        new UserFrame();
     }
 }
