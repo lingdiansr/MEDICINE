71a107820fcf9e366265f3f5fe19d5223fac20bd
HRruiH <2311465445@qq.com>
2024-01-18 09:26:35 +0800

    medicineMapper and medicineMapperImp

--- src/com/medicine/Mapper/medicineMapper.java
+++ src/com/medicine/Mapper/medicineMapper.java
@@ -1,4 +1,5 @@
 package com.medicine.Mapper;
+import com.medicine.Entity.Category;
 import com.medicine.Entity.Medicine;
 import com.medicine.Query.MedicineQuery;
 
@@ -8,10 +9,17 @@ public interface medicineMapper {
     boolean insert(Medicine m);
     boolean delete(Medicine m);
     boolean update(Medicine m);
+//    List<Medicine> select(Medicine m);
+
+    List<Medicine> selectByMedicineName(Medicine m);
 
     List<Medicine> selectAll();
     List<Medicine> fuzzySelect(String key);
 
-    String getSQLFromMedicineQuery(MedicineQuery query);
-    public List<Medicine> search(MedicineQuery query);
+    List<Medicine> selectByMedicinePrice(MedicineQuery m);
+
+    List<Medicine> selectByMedicinetype(MedicineQuery m);
+
+    List<Medicine> selectByMedicineDate(MedicineQuery m);
+
 }
